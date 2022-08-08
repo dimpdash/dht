006afc69ea24277f82dfd5681f65c39adc8628bc
dimpdash <Dimpdash@Gmail.com>
2022-08-08 19:09:38 +1000

    refactor out starting a raft bucket cluster on the same node

--- test/bucket_raft_test.exs
+++ test/bucket_raft_test.exs
@@ -2,17 +2,7 @@ defmodule DHT.BucketRaftTest do
   use ExUnit.Case
 
   setup do
-    nodes = [node()]
-    server_ids = [:ra_kv, node()]
-    {:ok, _, _} = DHT.BucketCluster.start(:ra_kv, nodes)
-
-    on_exit(fn ->
-      :ra.delete_cluster(server_ids)
-      for server_id <- server_ids, do: :ra.stop_server(server_id)
-          Process.sleep(1000)
-    end)
-
-    %{nodes: nodes, cluster: server_ids}
+    TestClusterHelper.same_node_cluster()
   end
 
   test "put and retrieve", %{cluster: cluster} do
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,22 @@
 ExUnit.start()
 ExUnited.start(false)
+
+defmodule TestClusterHelper do
+  use ExUnit.Case
+
+  def same_node_cluster() do
+    nodes = [node()]
+    server_ids = [:ra_kv, node()]
+    {:ok, _, _} = DHT.BucketCluster.start(:ra_kv, nodes)
+
+    on_exit(fn ->
+      :ra.delete_cluster(server_ids)
+      for server_id <- server_ids, do: :ra.stop_server(server_id)
+          Process.sleep(1000)
+    end)
+
+    %{nodes: nodes, cluster: server_ids}
+  end
+
+
+end
