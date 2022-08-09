5a1ab8ab45ec2132cefc2e766d9465db1b5a8e98
dimpdash <Dimpdash@Gmail.com>
2022-08-09 14:32:40 +1000

    refactor registry to use a bidirectional map

--- lib/registry.ex
+++ lib/registry.ex
@@ -1,12 +1,94 @@
-defmodule DHT.Registry do
-  defmodule State do
-    defstruct [
-      :buckets,
-      :refs,
-      :key_count, # num keys (int) -> cluster
-      bucket_keys: [],
-    ]
+defmodule RegistryState do
+  @moduledoc """
+  Implments a birdirectional map to find key bucket pairs along with keeping track of keys inserted in the bucket cluster
+  """
+  defstruct [
+    :keys_to_buckets,
+    :buckets_to_keys,
+    bucket_keys_count: [],
+  ]
+
+  def new() do
+    %RegistryState{
+      keys_to_buckets: Radix.new([]),
+      buckets_to_keys: %{},
+      bucket_keys_count: [],
+    }
   end
+
+  @doc"""
+  Lookup the cluster with the longer key prefix match
+
+  This is done with a Radix tree
+  """
+  def lookup_cluster(%RegistryState{keys_to_buckets: keys_to_buckets}, key) do
+    Radix.lookup(keys_to_buckets, key)
+  end
+
+  @doc"""
+  Get the key for a cluster
+  """
+  def get_key_prefix(%RegistryState{buckets_to_keys: buckets_to_keys}, cluster) do
+    Map.get(buckets_to_keys, cluster)
+  end
+
+  @doc"""
+  Increment the number of keys a bucket holds
+  """
+  def increment_keys(state = %{bucket_keys_count: bucket_keys_count}, key) do
+    {{count, val}, bucket_keys_count} = Find.take_first_match(bucket_keys_count, key)
+    count = count + 1
+    bucket_keys_count = [{count, val} | bucket_keys_count]
+    %{state | bucket_keys_count: bucket_keys_count}
+  end
+
+  @doc"""
+  Takes one of the maximum clusters with the maximum keys.
+  The cluster is then removed from the keys list
+  """
+  def take_max_key_cluster(state = %RegistryState{bucket_keys_count: bucket_keys_count}) do
+    [{from_key_count, key} | bucket_keys_count] = Enum.sort(bucket_keys_count, :desc)
+
+    state = %{state | bucket_keys_count: bucket_keys_count}
+
+    {state, key, from_key_count}
+  end
+
+  @doc"""
+  Add a bucket cluster
+  """
+  def add_bucket(state = %RegistryState{
+    bucket_keys_count: bucket_keys_count,
+    keys_to_buckets: keys_to_buckets,
+    buckets_to_keys: buckets_to_keys},
+    key, cluster, num_keys \\ 0) do
+
+    bucket_keys_count = [{num_keys, key} | bucket_keys_count]
+    buckets_to_keys = Map.put(buckets_to_keys, cluster, key)
+    keys_to_buckets = Radix.put(keys_to_buckets, [{key, cluster}])
+    %{state |
+      keys_to_buckets: keys_to_buckets,
+      bucket_keys_count: bucket_keys_count,
+      buckets_to_keys: buckets_to_keys
+    }
+  end
+
+  @doc"""
+  Fetch cluster from key
+  """
+  def fetch_cluster!(%RegistryState{keys_to_buckets: keys_to_buckets}, key) do
+    Radix.fetch!(keys_to_buckets, key)
+  end
+
+  @doc """
+  Get the number of buckets/keys
+  """
+  def size(%RegistryState{keys_to_buckets: keys_to_buckets}) do
+    Radix.count(keys_to_buckets)
+  end
+end
+
+defmodule DHT.Registry do
 
   use GenServer
 
@@ -16,7 +98,7 @@ defmodule DHT.Registry do
   Starts the registry
   """
   def start_link(opts) do
-    state = %State{}
+    state = RegistryState.new()
 
     GenServer.start_link(__MODULE__, %{state: state}, opts)
   end
@@ -36,7 +118,6 @@ defmodule DHT.Registry do
   """
 
   def put(server, key, value, :no_hash) do
-    Supervisor.
     GenServer.cast(server, {:put, key, value})
   end
 
@@ -62,17 +143,12 @@ defmodule DHT.Registry do
 
   @impl true
   def init(%{state: state}) do
-    bucket_keys = %{} #bucket pid to key
-
-    #create buckets
-    refs = %{} # refs to bucket pid
-    buckets = Radix.new([])
-    {:ok, %{state | buckets: buckets, refs: refs, bucket_keys: bucket_keys}}
+    {:ok, state}
   end
 
   @impl true
-  def handle_call({:get, key}, _from, state = %State{buckets: buckets}) do
-    with {_, bucket_cluster} <- Radix.lookup(buckets, key) do
+  def handle_call({:get, key}, _from, state = %RegistryState{}) do
+    with {_, bucket_cluster} <- RegistryState.lookup_cluster(state, key) do
       val = DHT.BucketRaft.get(bucket_cluster, key)
       {:reply, val, state}
     else
@@ -81,67 +157,59 @@ defmodule DHT.Registry do
   end
 
   @impl true
-  def handle_call({:add_bucket_cluster, cluster}, _from, state = %{buckets: buckets}) do
-    if Radix.count(buckets) == 0 do
-      buckets = Radix.put(buckets, <<>>, cluster)
-      bucket_keys = [{0, <<>>}]
-      state = %{state | buckets: buckets, bucket_keys: bucket_keys}
+  def handle_call({:add_bucket_cluster, to_cluster}, _from, state = %RegistryState{}) do
+    if RegistryState.size(state) == 0 do
+      state = RegistryState.add_bucket(state, <<>>, to_cluster, 0)
       {:reply, :ok, state}
     else
       # decide on cluster to split
       {state, key, from_key_count} = get_split_candidate(state)
-      {_, from_cluster} = Radix.fetch!(buckets, key)
+
+      {_, from_cluster} = RegistryState.fetch_cluster!(state, key)
 
       #new keys
       from_key = add_zero_bit(key)
       to_key = add_one_bit(key)
 
       # migrate keys
-      {:ok, key_moved_count} = DHT.BucketRaft.migrate_keys(cluster, from_cluster, to_key)
-      IO.puts "############################"
-      IO.inspect key_moved_count
+      {:ok, key_moved_count} = DHT.BucketRaft.migrate_keys(to_cluster, from_cluster, to_key)
+
       #Update the key priority tree
       to_key_count = key_moved_count #Assumes no keys in "to" bucket cluster
       from_key_count = from_key_count - key_moved_count
 
-      bucket_keys = state.bucket_keys
-      bucket_keys = [{from_key_count, from_key} | bucket_keys]
-      bucket_keys = [{to_key_count, to_key} | bucket_keys]
+      state = RegistryState.add_bucket(state, to_key, to_cluster, to_key_count)
+      state = RegistryState.add_bucket(state, from_key, from_cluster, from_key_count)
 
-      buckets = Radix.put(buckets, [{from_key, from_cluster},{to_key, cluster}])
-      {:reply, :ok, %{state | buckets: buckets, bucket_keys: bucket_keys}}
+      {:reply, :ok, state}
     end
   end
 
 
   @impl true
-  def handle_cast({:put, key, value}, state = %State{buckets: buckets}) do
+  def handle_cast({:put, key, value}, state = %RegistryState{}) do
     #get the partition it belongs in
+    IO.inspect state
 
-   with {bucket_key, bucket_cluster} <- Radix.lookup(buckets, key) do
+
+   with {bucket_key, bucket_cluster} <- RegistryState.lookup_cluster(state, key) do
       #update all buckets
       DHT.BucketRaft.put(bucket_cluster, key, value)
 
-      state = increment_list(state, bucket_key)
+      RegistryState.increment_keys(state, bucket_key)
+
       {:noreply, state}
     end
     {:noreply, state}
   end
 
-  defp increment_list(state = %State{bucket_keys: bucket_keys}, key) do
-    {{count, val}, bucket_keys} = Find.take_first_match(bucket_keys, key)
-    count = count + 1
-    bucket_keys = [{count, val} | bucket_keys]
-    %{state | bucket_keys: bucket_keys}
+  defp get_split_candidate(state = %RegistryState{}) do
+    RegistryState.take_max_key_cluster(state)
   end
 
-
-
-  defp get_split_candidate(state = %State{bucket_keys: bucket_keys}) do
-    [{from_key_count, key} | bucket_keys] = Enum.sort(bucket_keys, :desc)
-
-    {%{state | bucket_keys: bucket_keys}, key, from_key_count}
-  end
+  # def remove_cluster(cluster, state = %RegistryState{}) do
+  #   key
+  # end
 
   # def merge_buckets(key, state = %State{buckets: buckets}) do
   #   keys_to_merge = Radix.more(buckets, key)
@@ -215,7 +283,6 @@ defmodule Find do
     i = Enum.find_index(collection, fn({_, element2}) ->
       element2 == item
     end)
-
     List.pop_at(collection, i)
   end
 
